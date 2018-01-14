fa554884b756da25f66e1e3e7957666a41783d31
Chelsea Roman <chelsea.ann.roman@gmail.com>
2018-01-14 01:06:45 -0500

    lesson 3

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,8 @@
-// let's go!
+import React from 'react';
+import {render} from 'react-dom';
+//imports the render method instead of the entire react-dom package
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker/>, document.querySelector('#main'));
+
+//document.querySelector returns the first Elements that matches id of main or group of them
