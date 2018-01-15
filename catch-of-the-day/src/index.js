31d0493df68ad5476ebb6b3b4d24626bd030d35b
Chelsea Roman <chelsea.ann.roman@gmail.com>
2018-01-15 16:15:33 -0500

    components order and inventory added

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './css/style.css';
+import App from './components/App';
 //imports the render method instead of the entire react-dom package
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
 
 //document.querySelector returns the first Elements that matches id of main or group of them
